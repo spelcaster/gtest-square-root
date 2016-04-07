554f10d9c2b93fc94908512b58a8a1f38c2e952d
Hugo Augusto Freitas do Carmo <hugrito.kun@gmail.com>
2016-04-07 16:02:03 -0300

    Modifying test suite names
    - The test suite name now represents the function that we are testing;

--- tests/functions_test.cpp
+++ tests/functions_test.cpp
@@ -2,18 +2,18 @@
 
 #include "../include/functions.hpp"
 
-TEST (SquareRootTest, PositiveNos) {
+TEST (SquareRootFunction, PositiveNos) {
     EXPECT_EQ (18.0, square_root(324.0));
     EXPECT_EQ (25.4, square_root(645.16));
     EXPECT_EQ (50.3321, square_root(2533.310224));
 }
 
-TEST (SquareRootTest, ZeroAndNegativeNos) {
+TEST (SquareRootFunction, ZeroAndNegativeNos) {
     ASSERT_EQ(0.0, square_root(0.0));
     ASSERT_EQ(-1, square_root(-22.0));
 }
 
-TEST (DieTest, CustomExitFunction) {
+TEST (DieFunction, CustomExitFunction) {
     // Run death tests in thread safe mode
     ::testing::FLAGS_gtest_death_test_style = "threadsafe";
 
