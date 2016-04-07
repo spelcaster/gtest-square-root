2d880a433b5a709744c4d909262e1e2d0ea783f3
Hugo Augusto Freitas do Carmo <hugrito.kun@gmail.com>
2016-04-07 16:12:21 -0300

    Adding SquareRoot in the main function
    - Example on how SquareRoot can be used in a program;

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,20 @@
 #include "../include/functions.hpp"
+#include "../include/SquareRoot.hpp"
 
 #include <iostream>
 
 int main (int argc, char **argv) {
-
     std::cout << square_root(-9.0) << std::endl;
     std::cout << square_root(0.0) << std::endl;
     std::cout << square_root(9.0) << std::endl;
+
+    SquareRoot root;
+    root.setNumber(-9.0);
+    std::cout << root.calculate() << std::endl;
+    root.setNumber(0.0);
+    std::cout << root.calculate() << std::endl;
+    root.setNumber(9.0);
+    std::cout << root.calculate() << std::endl;
 
     return 0;
 }
